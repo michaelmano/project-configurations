13934c6da50916de64d01cbba3b1de6b6d3fd87a
Michael Mano <michael.mano26@gmail.com>
2018-04-19 09:53:23 +1000

    updates dependancies and adds additional configs

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,6 @@ let mix = require("laravel-mix");
 
 mix
   .disableNotifications()
-  .js("resources/js/app.js", "app/js/app.js")
-  .sass("resources/sass/app.scss", "app/css/app.css")
-  .copy("resources/images/", `./app/images`, true);
+  .js("resources/js/app.js", "src/assets/js/app.js")
+  .sass("resources/sass/app.scss", "src/assets/css/app.css")
+  .copy("resources/images/", `./src/assets/images`, true);
